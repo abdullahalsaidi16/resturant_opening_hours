b5d6a13a022c399c7f309728ac2016263d22b904
Abdullah Al Saidi <abdullah.al.saidi@bitbucket.com>
2019-09-22 13:56:18 +0300

    heroku dependcies

--- rest_hours/settings.py
+++ rest_hours/settings.py
@@ -129,3 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Activate Django-Heroku.
+import django_heroku
+django_heroku.settings(locals())
