cfdd298266eebbd2b1d4ed3720a78c4852f2252a
Abdullah Al Saidi <abdullah.al.saidi@bitbucket.com>
2019-09-22 14:33:30 +0300

    config for heroku db

--- rest_hours/settings.py
+++ rest_hours/settings.py
@@ -134,3 +134,7 @@ STATIC_URL = '/static/'
 # Activate Django-Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
